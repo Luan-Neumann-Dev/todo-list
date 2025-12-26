ba748afed53b67fe891ea2b4def71b6a82bb97db
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-12-26 13:49:41 -0300

    feat: :sparkles: card component

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Button from "./components/button";
 import ButtonIcon from "./components/button-icon";
 import InputText from "./components/input-text";
 import InputCheckbox from "./components/input-checkbox";
+import Card from "./components/card";
 
 export function App() {
   return (
@@ -56,6 +57,10 @@ export function App() {
 
       <div>
         <InputCheckbox />
+      </div>
+
+      <div>
+        <Card size='md'>Olá mundo</Card>
       </div>
     </div>
   )
