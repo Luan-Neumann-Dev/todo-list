f7663785cbe289c1f349c4d5b82af8f4f1f96787
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-12-26 10:51:02 -0300

    feat: :sparkles: icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Text from "./components/text";
+import TrashIcon from './assets/icons/trash.svg?react';
 
 export function App() {
   return (
@@ -6,6 +7,8 @@ export function App() {
        <Text variant='body-sm-bold' className="text-pink-base">Olá mundo!</Text>
        <Text variant='body-md-bold' className="text-green-base">Olá mundo!</Text>
        <Text variant='body-md'>Olá mundo!</Text>
+
+       
     </div>
   )
 }
